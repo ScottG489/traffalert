3316e0aef5d34dadcb1c9c3fb08278421e5b50a8
Scott G <ScottG489@Gmail.com>
2012-05-23 09:42:53 -0400

    Organize db.Model objects and their utility functions into models module. - Move data storage related classes and functions into models module. - Fix bug resulting from no days being selected. Error is now displayed. - Fix bug resulting in days selection not persisting on form.

--- src/main.py
+++ src/main.py
@@ -2,13 +2,13 @@
 #from google.appengine.ext.webapp2.util import run_wsgi_app
 import webapp2
 from google.appengine.api import users
-from google.appengine.ext import db
 import logging
 import os
 import jinja2
 import cgi
 import datetime
 import traffic
+import models
 from crons import CronJobs
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
@@ -22,35 +22,18 @@ class PageHandler(webapp2.RequestHandler):
     def write(self, string):
         self.response.out.write(string)
 
-
 class MainPage(PageHandler):
-    def get_existing_user(self, openid_user):
-        logging.info('Getting existing user')
-        existing_user = db.GqlQuery('SELECT * FROM User WHERE user_id = :1', 
-                str(openid_user.user_id()))
-        existing_user = list(existing_user)
-        if existing_user:
-            logging.info('Existing user user_id: ' + existing_user[0].user_id)
-            return existing_user[0]
-        logging.info('User doesn\'t exist')
-
-    def create_new_user(self, openid_user):
-        logging.info('Creating new user')
-        user = User(user_id = openid_user.user_id(), nickname =
-                openid_user.nickname(), email = openid_user.email())
-        user.put()
-        return user
-
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
 
+        models_handler = models.ModelsHandler()
         openid_user = users.get_current_user()
         if openid_user:  # signed in already
-            existing_user = self.get_existing_user(openid_user)
+            existing_user = models_handler.get_existing_user(openid_user)
 
             user = None
             if not existing_user:
-                user = self.create_new_user(openid_user)
+                user = models_handler.create_new_user(openid_user)
             else:
                 user = existing_user
 
@@ -59,10 +42,51 @@ class MainPage(PageHandler):
 
             self.write_template('main_page.html')
 
-            route_data = self.get_user_route_data(user)
+            route_data = models_handler.get_user_route_data(user)
             self.write_user_route_data(route_data)
 
         else:     # let openid_user choose authenticator
+            self.response.out.write('Hello world! Sign in at: ')
+            self.write('<a href="%s" >login</a><br />' %
+                    users.create_login_url(federated_identity='www.google.com/accounts/o8/id'))
+
+    def post(self):
+        openid_user = users.get_current_user()
+        if openid_user:  # signed in already
+            models_handler = models.ModelsHandler()
+            existing_user = models_handler.get_existing_user(openid_user)
+
+            user = None
+            if not existing_user:
+                user = models_handler.create_new_user(openid_user)
+            else:
+                user = existing_user
+
+            self.response.out.write('Hello <em>%s</em>! [<a href="%s">sign out</a>]' % (
+                    user.nickname, users.create_logout_url(self.request.uri)))
+
+            inputs = self.get_inputs()
+            errors = self.get_input_errors(inputs)
+            params = {}
+            params.update(inputs)
+            params.update(errors)
+
+            routes = None
+            if not errors:
+                router = traffic.RouteHandler()
+                routes = router.get_routes(inputs.get('start'),
+                        inputs.get('end'))
+
+                route = models_handler.put_route(user, routes[0])
+                time = datetime.time(int(inputs['hour']), int(inputs['minute']))
+                models_handler.put_alert(route, inputs['days'], time)
+
+
+            route_data = models_handler.get_user_route_data(user)
+            self.write_template('main_page.html', **params)
+            self.write_user_route_data(route_data)
+
+        else:     # TODO: let openid_user choose authenticator
             self.response.out.write('Hello world! Sign in at: ')
             self.write('<a href="%s" >login</a><br/>' %
                     users.create_login_url(federated_identity='www.google.com/accounts/o8/id'))
@@ -79,84 +103,12 @@ class MainPage(PageHandler):
 
         return inputs
 
-    def post(self):
-        openid_user = users.get_current_user()
-        if openid_user:  # signed in already
-            existing_user = self.get_existing_user(openid_user)
-
-            user = None
-            if not existing_user:
-                user = self.create_new_user(openid_user)
-            else:
-                user = existing_user
-
-            self.response.out.write('Hello <em>%s</em>! [<a href="%s">sign out</a>]' % (
-                    user.nickname, users.create_logout_url(self.request.uri)))
-
-            inputs = self.get_inputs()
-
-            errors = self.validate_input(inputs)
-
-            params = {}
-            params.update(inputs)
-            params.update(errors)
-
-            routes = ''
-            if not errors:
-                router = traffic.RouteHandler()
-                routes = router.get_routes(inputs.get('start'),
-                        inputs.get('end'))
-            else:
-                self.write_template('main_page.html', **params)
-                return
-
-            self.write_template('main_page.html', **params)
-
-            route = self.put_route(user, routes[0])
-            time = datetime.time(int(inputs['hour']), int(inputs['minute']))
-            self.put_alert(route, inputs['days'], time)
-
-            route_data = self.get_user_route_data(user)
-
-            self.write_user_route_data(route_data)
-
-
-        else:     # TODO: let openid_user choose authenticator
-            self.response.out.write('Hello world! Sign in at: ')
-            self.write('<a href="%s" >login</a><br/>' %
-                    users.create_login_url(federated_identity='www.google.com/accounts/o8/id'))
-
     def write_user_route_data(self, route_data):
         for route in route_data:
             self.write_route(route['route'])
             for alert in route['alerts']:
                 self.write_alert(alert)
 
-    # TODO: Should make objects for user, route, and alarm that's separate from
-    # the models? This essentially makes a dict of user and all it's data, no
-    # concrete structure like an object.
-    def get_user_route_data(self, user):
-        user_data = []
-
-        routes = self.get_routes(user)
-        routes.reverse()
-        for route in routes:
-            route_data = {}
-
-            alerts = self.get_alerts(route)
-            alerts.reverse()
-
-            route_data['route'] = route
-            route_data['alerts'] = alerts
-
-            user_data.append(route_data)
-
-        return user_data
-
-    def put_alert(self, route, days, time):
-        alert_model = Alert(route = route, days = days, time = time)
-        alert_model.put()
-        return alert_model
 
     def write_route(self, route):
         #self.write(str(route) + '<br />')
@@ -167,40 +119,19 @@ class MainPage(PageHandler):
         self.write('<b>Duration: </b>' + route.normal_duration + '<br /><br />')
 
     def write_alert(self, alert):
-        #self.write(str(alert) + '<br />')
         self.write('<div style="margin-left: 100px">')
-        self.write('<b>Route: </b>' + str(alert.route) + '<br />')
         self.write('<b>Days: </b>' + str(alert.days) + '<br />')
         self.write('<b>Time: </b>' + str(alert.time) + '<br /><br />')
         self.write('</div>')
 
-
-    def put_route(self, user, route):
-        route_model = Route(user = user, start = route.start, end = route.end, name = route.name, distance =
-                route.distance, normal_duration = route.normal_duration)
-        route_model.put()
-
-        return route_model
-
-
-    def get_alerts(self, route):
-        logging.info('Getting alerts for route: ' + str(route))
-        alerts = db.GqlQuery('SELECT * FROM Alert WHERE route = :1',
-                route)
-        return list(alerts)
-
-    def get_routes(self, user):
-        logging.info('Getting routes for user: ' + str(user))
-        routes = db.GqlQuery('SELECT * FROM Route WHERE user = :1',
-                user)
-        return list(routes)
-
-    def validate_input(self, inputs):
+    def get_input_errors(self, inputs):
         errors = {}
         if not inputs.get('start'):
             errors['start_error'] = 'Required field'
         if not inputs.get('end'):
             errors['end_error'] = 'Required field'
+        if not inputs.get('days'):
+            errors['days_error'] = 'Must select at least 1 day'
         if not self.is_valid_time(inputs.get('hour'), inputs.get('minute')):
             errors['time_error'] = 'Invalid time'
 
@@ -219,26 +150,6 @@ class MainPage(PageHandler):
     def escape_html(self, string):
             return cgi.escape(string, quote=True)
 
-
-class User(db.Model):
-    user_id = db.StringProperty(required=True)
-    nickname = db.StringProperty(required=True)
-    email = db.EmailProperty()
-
-class Route(db.Model):
-    user = db.ReferenceProperty(User, required=True)
-    start = db.StringProperty(required=True)
-    end = db.StringProperty(required=True)
-    name = db.StringProperty(required=True)
-    distance = db.StringProperty(required=True)
-    normal_duration = db.StringProperty(required=True)
-#    traffic_duration = db.StringProperty()
-#    traffic_color = db.StringProperty()
-
-class Alert(db.Model):
-    route = db.ReferenceProperty(Route, required=True)
-    days = db.ListProperty(str, required=True)
-    time = db.TimeProperty(required=True)
 
 app = webapp2.WSGIApplication([('/', MainPage),
                             ('/crons', CronJobs)],
---
+++ src/models.py
@@ -0,0 +1,85 @@
+from google.appengine.ext import db
+import logging
+
+class ModelsHandler(object):
+    def get_existing_user(self, openid_user):
+        logging.info('Getting existing user')
+        existing_user = db.GqlQuery('SELECT * FROM User WHERE user_id = :1', 
+                str(openid_user.user_id()))
+        existing_user = list(existing_user)
+        if existing_user:
+            logging.info('Existing user user_id: ' + existing_user[0].user_id)
+            return existing_user[0]
+        logging.info('User doesn\'t exist')
+
+    def get_alerts(self, route):
+        logging.info('Getting alerts for route: ' + str(route))
+        alerts = db.GqlQuery('SELECT * FROM Alert WHERE route = :1',
+                route)
+        return list(alerts)
+
+    def get_routes(self, user):
+        logging.info('Getting routes for user: ' + str(user))
+        routes = db.GqlQuery('SELECT * FROM Route WHERE user = :1',
+                user)
+        return list(routes)
+
+    # TODO: Should make objects for user, route, and alarm that's separate from
+    # the models? This essentially makes a dict of user and all it's data, no
+    # concrete structure like an object.
+    def get_user_route_data(self, user):
+        user_data = []
+
+        routes = self.get_routes(user)
+        routes.reverse()
+        for route in routes:
+            route_data = {}
+
+            alerts = self.get_alerts(route)
+            alerts.reverse()
+
+            route_data['route'] = route
+            route_data['alerts'] = alerts
+
+            user_data.append(route_data)
+
+        return user_data
+
+    def create_new_user(self, openid_user):
+        logging.info('Creating new user')
+        user = User(user_id = openid_user.user_id(), nickname =
+                openid_user.nickname(), email = openid_user.email())
+        user.put()
+        return user
+
+    def put_alert(self, route, days, time):
+        alert_model = Alert(route = route, days = days, time = time)
+        alert_model.put()
+        return alert_model
+
+    def put_route(self, user, route):
+        route_model = Route(user = user, start = route.start, end = route.end, name = route.name, distance =
+                route.distance, normal_duration = route.normal_duration)
+        route_model.put()
+
+        return route_model
+
+class User(db.Model):
+    user_id = db.StringProperty(required=True)
+    nickname = db.StringProperty(required=True)
+    email = db.EmailProperty()
+
+class Route(db.Model):
+    user = db.ReferenceProperty(User, required=True)
+    start = db.StringProperty(required=True)
+    end = db.StringProperty(required=True)
+    name = db.StringProperty(required=True)
+    distance = db.StringProperty(required=True)
+    normal_duration = db.StringProperty(required=True)
+#    traffic_duration = db.StringProperty()
+#    traffic_color = db.StringProperty()
+
+class Alert(db.Model):
+    route = db.ReferenceProperty(Route, required=True)
+    days = db.ListProperty(str, required=True)
+    time = db.TimeProperty(required=True)
